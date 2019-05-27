fe86dae58a09b7e3359ddefd0790fc621181bc10
Laros <ing.arellano@gmail.com>
2019-05-27 22:01:34 +0200

    actualizando versiones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'meuprojecto';
+  title = 'Meu Proyecto';
+  nombre = 'Luis AR';
 }
