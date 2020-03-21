a44b6c3f83876e8b648664dcb63d7487b4f78306
Laros <ing.arellano@gmail.com>
2020-03-21 13:37:46 +0100

    9.9. Usando property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nombre = 'Luis Aros.';
 
-  public adicionar(nombre: string){
-    console.log(nombre);
-    
-    this.nombre = nombre;
-
+  public alterarNome(evento: any){
+    console.log(evento);
+    this.nombre=evento.target.value;
+  }
+  public adicionar(){
     console.log(`Agregando ${this.nombre}`);
+    const numero = Math.round(Math.random()*100);
+    this.nombre = this.nombre + numero;
   }
 }
