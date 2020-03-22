a8714d1fa55ec04d7b40b4a8af24a3ea1ecab8a5
Laros <ing.arellano@gmail.com>
2020-03-22 12:13:43 +0100

    9.17. Estilos CSS dinâmicos com ngStyle

--- src/app/trabajador-card/trabajador-card.component.ts
+++ src/app/trabajador-card/trabajador-card.component.ts
@@ -15,4 +15,12 @@ export class TrabajadorCardComponent{
 
   @Input('inputTrabajador') trabajador: any;
 
+  getEstilosTarjeta() {
+    return {
+      //'background-color': 'red'
+      backgroundColor: this.trabajador.id % 2 == 0 ? 'blue' : 'lightgreen',
+      //'border-width': this.trabajador.id + 'px'
+      'border-width.px': this.trabajador.id
+    };
+  }
 }
