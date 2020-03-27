d0f43478064fdd00385ff35f2ebaac8ef4e92756
Laros <ing.arellano@gmail.com>
2020-03-27 09:49:38 +0100

    12.6. Two-way binding com ngModel

--- src/app/formularios/formularios.component.ts
+++ src/app/formularios/formularios.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+class Cliente {
+  nombre: string;
+  email: string;
+  profesion: string;
+}
+
+
 @Component({
   selector: 'app-formularios',
   templateUrl: './formularios.component.html',
@@ -13,14 +20,24 @@ export class FormulariosComponent {
     'Empresario',
     'Otra'
   ];
+
   profesionDefalt = 'Programador';
+
+  cliente: Cliente = new Cliente();
 
   guardar(usuarioForm: NgForm) {
     console.log('Salvando...');
     console.log(usuarioForm);
-    console.log(usuarioForm.value.nombre);// acceder al valor del campo nombre
-
+    console.log(usuarioForm.value.nombre); // acceder al valor del campo nombr
     console.log(usuarioForm.value.profesion);
+
+/*  Se puede hacer tb con un binding en el campo-> [(ngModel)]
+    this.cliente.nombre = usuarioForm.value.nombre;
+    this.cliente.email = usuarioForm.value.email;
+    this.cliente.profesion = usuarioForm.value.profesion;
+*/
+    console.log(usuarioForm.value);
+    console.log(this.cliente);
   }
 
 }
